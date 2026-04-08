2b4d4ffd366b7b801cf54bea6796fd603bf1e2e7
nageshkumar-gh <nagesh.kumar.ie@gmail.com>
2026-04-08 11:02:14 +0100

    login TC

---
+++ tests/Login/login.spec.ts
@@ -0,0 +1,12 @@
+import test,{expect} from '@playwright/test';
+
+test("valid login",
+    async({page})=>
+    {
+        await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        await page.getByPlaceholder('Username').fill("Admin");
+        await page.getByPlaceholder('Password').fill("admin123");
+        await page.getByRole('button',{name:'Login'}).click();
+        await expect(page.getByRole('heading', { name: "Dashboard" })).toBeVisible();
+    }
+)
