9d8065a34f8dd215de0c4603d9a4a17b8de4ba22
nageshkumar-gh <nagesh.kumar.ie@gmail.com>
2026-04-09 06:44:48 +0100

    login scenarios

--- tests/Login/login.spec.ts
+++ tests/Login/login.spec.ts
@@ -22,3 +22,45 @@ test("User sees error message with invalid username and password",
         await expect(page.getByText('Invalid credentials')).toBeVisible();
     }
 )
+
+test("User cannot login with empty credentials",
+    async({page})=>
+    {
+        await page.goto("/");
+        await page.getByPlaceholder('Username').fill(loginData.emptyUser.username);
+        await page.getByPlaceholder('Password').fill(loginData.emptyUser.password);
+        await page.getByRole('button',{name:'Login'}).click();
+        await expect(page.getByText('Required')).toHaveCount(2);
+    }
+)
+
+test("Admin can logout successfully",
+    async({page})=>
+    {
+        await page.goto("/");
+        await page.getByPlaceholder('Username').fill(process.env.USERNAME!);
+        await page.getByPlaceholder('Password').fill(process.env.PASSWORD!);
+        await page.getByRole('button',{name:'Login'}).click();
+        await expect(page.getByRole('heading', { name: "Dashboard" })).toBeVisible();
+
+        await page.locator("//span[@class='oxd-userdropdown-tab']/i").click();
+        await page.getByText('Logout').click();
+    }
+)
+
+test("Session expired redirects to login",
+    async({page,context})=>
+    {
+    
+        await page.goto("/");
+        await page.getByPlaceholder('Username').fill(process.env.USERNAME!);
+        await page.getByPlaceholder('Password').fill(process.env.PASSWORD!);
+        await page.getByRole('button',{name:'Login'}).click();
+        await expect(page.getByRole('heading', { name: "Dashboard" })).toBeVisible();
+
+        await context.clearCookies();
+        await page.reload();
+
+        await expect(page.getByRole('heading',{name:'Login'})).toBeVisible();
+    }
+)
